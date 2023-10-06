741049d1268e165f025f41b685d99e49bd324fa5
Bohao Cao <bohao.cao@gmail.com>
2023-10-05 21:34:41 -0400

    reorder code

--- jobs/create_job.sh
+++
@@ -1,8 +0,0 @@
-gcloud run jobs create job-quickstart \
-    --image gcr.io/upwork-ocean-classification/logger-job \
-    --tasks 1 \
-    --set-env-vars file_name=df_buy_insta_accounts_add_on.tsv \
-    --set-env-vars sleep=30 \
-    --max-retries 1 \
-    --region us-central1 \
-    --project=upwork-ocean-classification
